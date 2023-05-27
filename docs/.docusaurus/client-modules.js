e410dfb3e1c03ff719e8502954c1678b48c36e72
Serge van den Oever <serge.van.den.oever@macaw.nl>
2023-05-28 00:25:17 +0200

    WIP

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/svdoever/p/ai-writer/docs/node_modules/infima/dist/css/default/default.css'),
+  require('/home/svdoever/p/ai-writer/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/svdoever/p/ai-writer/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/svdoever/p/ai-writer/docs/src/css/custom.css'),
+];
