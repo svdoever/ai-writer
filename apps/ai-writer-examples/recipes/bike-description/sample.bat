805f75a296f07cce7ee8bbd494d0d959981c3d23
Serge van den Oever <serge.van.den.oever@macaw.nl>
2023-06-04 00:48:47 +0200

    Update the bike-description recipe

---
+++ apps/ai-writer-examples/recipes/bike-description/sample.bat
@@ -0,0 +1,1 @@
+npx ai-writer bike-description --name "VanMoof S3" --type "Electric" --range "90" --output "bikes/VanMoof-S3" --verbose
---
+++ apps/ai-writer-examples/recipes/bike-description/sample.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+
+npx ai-writer bike-description --name "VanMoof S3" --type "Electric" --range "90" --output "bikes/VanMoof-S3" --verbose
